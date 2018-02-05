ac464d4bc45f00b0382bc5b46c1161525bf43934
VladDekht <vladislav.dekhtiarenko@yandex.ru>
2018-02-05 15:29:47 +0300

    Services

---
+++ Webmap 1A/Services/DataAccessService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Webmap_1A.Models;
+
+namespace Webmap_1A.Services
+{
+    public class DataAccessService : DbContext
+    {
+        public DbSet<Order> Orders { get; set; } 
+    }
+}
---
+++ Webmap 1A/Services/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webmap_1A.Models;
+using Webmap_1A.Services;
+
+namespace Webmap_1A
+{
+    public class OrderRepository
+    {
+        private static List<Order> _orders;
+
+        public OrderRepository()
+        {
+            //_orders = new List<Order>{ new Order { Lat = 55F, Lng = 27.56667F, Id = 0 } };
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            using (var db = new DataAccessService())
+            {
+                return db.Orders.ToList();
+            }
+        }
+
+        public async Task<Order> GetOrder(int id)
+        {
+            return await Task.Run(()=> _orders.FirstOrDefault(f => f.Id == id));
+        }
+
+        public void AddOrder(Order order)
+        {
+            _orders.Add(order);
+        }
+    }
+}
