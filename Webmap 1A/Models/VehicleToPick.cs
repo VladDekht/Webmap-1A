45f9bd27a251dd6bcb333376a9bb7904ba86d354
VladDekht <vladislav.dekhtiarenko@yandex.ru>
2018-02-05 15:24:04 +0300

    2

---
+++ Webmap 1A/Models/VehicleToPick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webmap_1A.Models
+{
+    public class VehicleToPick
+    {
+        public string Diagnose { get; set; }
+        public string Type { get; set; }
+        public string PlateNum { get; set; }
+        public string OtherInfo { get; set; }
+    }
+}
---
+++ Webmap 1A/Models/Wrecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Webmap_1A.Models
+{
+    public class Wrecker
+    {
+        [Key]
+        public int WreckerId { get; set; }
+        public string PlateNum { get; set; }
+        public Driver CurrentDriver { get; set; }
+    }
+}
