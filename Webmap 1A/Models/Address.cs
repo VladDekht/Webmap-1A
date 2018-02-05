504eaa016f888031112b23fe992fd81c10015d2d
VladDekht <vladislav.dekhtiarenko@yandex.ru>
2018-02-05 15:22:53 +0300

    1

---
+++ Webmap 1A/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webmap_1A.Models
+{
+    public class Address
+    {
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int HouseNum { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ Webmap 1A/Models/Caller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webmap_1A.Models
+{
+    public enum CallerType { Individual, Cop }
+    public class Caller
+    {
+        public CallerType Type { get; set; }
+        public string Name { get; set; }
+        public string PhoneNum { get; set; }
+    }
+}
