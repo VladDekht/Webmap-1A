53c817ad059ef9efe7b6818b653511f61b4f5dd2
Yura <yura.varlakov@gmail.com>
2018-02-08 11:32:42 +0300

    Order creation form

--- Webmap 1A/Scripts/app/webmap.viewmodel.js
+++ Webmap 1A/Scripts/app/webmap.viewmodel.js
@@ -1,1 +1,33 @@
-﻿
+﻿function AddOrderViewModel(app, dataModel) {
+	var self = this;
+
+	self.otherInfo = ko.observable("Info");
+
+	saveChanges = function () {
+		debugger;
+		//$.post( "../api/me/order", $( "#fNewOrder" ).serialize() );
+		var jsonObj = ko.mapping.toJS(self);
+
+		$.ajax({
+			method: 'post',
+			url: "../api/me/order",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify(jsonObj),
+			dataType: "json",
+			//headers: {
+			//	'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
+			//},
+			success: function (data) {
+				alert("DONE");
+			}
+		});
+	}
+
+	return self;
+}
+
+app.addViewModel({
+	name: "AddOrder",
+	bindingMemberName: "newOrder",
+	factory: AddOrderViewModel
+});
